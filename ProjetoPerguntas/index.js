c0821ec26b7d5decbfe2a19d193ee52b6360160d
DAYVSON SILVINO DA SILVA <62804086+21121990@users.noreply.github.com>
2020-04-20 20:57:37 -0300

    Projeto Pesquisa Node.js

---
+++ ProjetoPerguntas/index.js
@@ -0,0 +1,55 @@
+const express = require("express"); // Importando o express
+const app = express(); // Iniciando o express
+const bodyParser = require("body-parser") //requisições post
+const connection = require("./database/database"); //Instanciando database
+const Pergunta = require("./database/Pergunta");
+//database
+
+connection
+    .authenticate()
+    .then(()=>{
+         console.log("conexão feita com o banco de dados")
+    })
+    .catch((msgErro)=>{
+        console.log(msgErro);
+    })
+
+
+//Express utiliza ejs como view engine
+app.set('view engine','ejs');
+app.use(express.static('public'));
+//Body parser para requisição post
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+//Rotas
+app.get("/",(req,res)=>{
+    Pergunta.findAll({raw:true, order:[
+      ['id','desc'] //ordenar findAll asc= crescente || desc= decrescente  
+    ]}).then(perguntas =>{
+        res.render("index",{
+            perguntas: perguntas
+        });
+    });
+  
+});
+
+app.get("/pergunta",(req,res)=>{
+    res.render("pergunta")
+});
+
+app.post("/salvarpergunta",(req,res)=>{
+    var titulo = req.body.titulo;
+    var descricao = req.body.descricao;
+    Pergunta.create({
+        titulo: titulo,
+        descricao: descricao
+    }).then(() =>{
+        res.redirect("/");
+    });
+});
+
+//iniciando servidor
+app.listen(4000,()=>
+{
+    console.log("Servidor online");
+});
